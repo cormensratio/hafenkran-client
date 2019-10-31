efbe6223c9712774de5f208f596661427b99774a
Johannes Pannermayr <pannermayr@fim.uni-passau.de>
2019-10-31 14:11:34 +0100

    EPDS02-31 Created basic UploadedDockerfilesPage

---
+++ src/components/views/UploadedDockerfilesPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <base-page>
+    <v-data-table></v-data-table>
+  </base-page>
+</template>
+
+<script>
+import BasePage from '../baseComponents/BasePage';
+
+export default {
+  name: 'UploadedDockerfilesPage',
+  components: { BasePage },
+};
+</script>
+
+<style scoped>
+
+</style>
