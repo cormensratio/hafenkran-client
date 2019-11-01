590ccf17bcd72e1c01988c8a4d09761959a3bbfd
RGB42 <40913227+RGB@users.noreply.github.com>
2019-11-01 13:02:33 +0100

    EPDS02-27 Timestamp on upload is saved now

--- src/components/baseComponents/FileUpload.vue
+++ src/components/baseComponents/FileUpload.vue
@@ -4,6 +4,8 @@
       <label><strong id="uploadtext">Upload your Dockerfile here:</strong><br>
         <input type="file" ref="file" id="file" @change="getFile"/>
         <v-btn v-on:click="submitFile()">Submit</v-btn>
+        <v-btn v-on:click="test">klick</v-btn>
+        <p v-if="timestamp!=null">Successfully uploaded at:{{timestamp}}</p>
       </label>
     </div>
   </div>
@@ -17,17 +19,31 @@ export default {
   name: 'FileUpload',
   data() {
     return {
-      file: '',
+      datapack: [{}],
+      file: null,
+      timestamp: null,
     };
   },
   methods: {
+    test() {
+      console.log(this.datapack);
+    },
     getFile() {
+      const today = new Date();
+      const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+      const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+      const dateTime = `${date} ${time}`;
+      this.timestamp = dateTime;
       this.file = this.$refs.file.files[0];
+      this.datapack[0] = this.file;
+      this.datapack[1] = this.timestamp;
+      console.log(this.file);
+      console.log(this.file.name);
     },
     submitFile() {
       const formData = new FormData();
-      formData.append('file', this.file);
-      ApiService.doPost('/Service', formData, {
+      formData.append('datapack', this.datapack);
+      ApiService.doPost('/INSERT_ENDPOINT_HERE', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -43,7 +59,6 @@ export default {
     margin-top: 10%;
     background-color: beige;
   }
-
   #uploadtext {
     margin: auto;
   }
