59341c3420ff753b44929d0ad159459fe1dd8968
RGB42 <40913227+RGB@users.noreply.github.com>
2019-11-01 22:46:41 +0100

    EPDS02-27 Implementing name editing ability

--- src/components/baseComponents/FileUpload.vue
+++ src/components/baseComponents/FileUpload.vue
@@ -5,7 +5,8 @@
         <input type="file" ref="file" id="file" @change="getFile"/>
         <v-btn v-on:click="submitFile()">Submit</v-btn>
         <v-btn v-on:click="test">klick</v-btn>
-        <p v-if="timestamp!=null">Successfully uploaded at:{{timestamp}}</p>
+        <p v-if="timestamp!=null">Successfully uploaded at:{{timestamp}}</p><br>
+        <input id="name" ref="name" type="text" @change="updateFileName" :value="fileName"/>
       </label>
     </div>
   </div>
@@ -22,21 +23,29 @@ export default {
       datapack: [{}],
       file: null,
       timestamp: null,
+      fileName: null,
     };
   },
   methods: {
     test() {
       console.log(this.datapack);
     },
+    updateFileName() {
+      this.fileName = this.$refs.name.name;
+    },
     getFile() {
+      // create timestamp
       const today = new Date();
       const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
       const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-      const dateTime = `${date} ${time}`;
-      this.timestamp = dateTime;
+      this.timestamp = `${date} ${time}`;
+      // fill datapack with data
       this.file = this.$refs.file.files[0];
+      this.fileName = this.file.name;
       this.datapack[0] = this.file;
       this.datapack[1] = this.timestamp;
+      this.datapack[2] = this.fileName;
+      // debugging
       console.log(this.file);
       console.log(this.file.name);
     },
