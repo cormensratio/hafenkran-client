096f6743c7f3f6570667e8e4279524cbc79501a6
Johannes Pannermayr <pannermayr@fim.uni-passau.de>
2019-11-03 17:27:34 +0100

    EPDS02-31 Added example table to UploadedDockerFilesPage

--- src/components/views/UploadedDockerfilesPage.vue
+++ src/components/views/UploadedDockerfilesPage.vue
@@ -1,6 +1,17 @@
 <template>
   <base-page>
-    <v-data-table></v-data-table>
+      <template slot="body">
+        <div id="app">
+          <v-app id="inspire">
+            <v-data-table
+              :headers="headers"
+              :items="uploadedDockerImages"
+              :items-per-page="10"
+              class="elevation-1"
+            ></v-data-table>
+          </v-app>
+        </div>
+      </template>
   </base-page>
 </template>
 
@@ -10,6 +21,37 @@ import BasePage from '../baseComponents/BasePage';
 export default {
   name: 'UploadedDockerfilesPage',
   components: { BasePage },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Dockerfile Name',
+          align: 'left',
+          sortable: true,
+          value: 'name',
+        },
+        { text: 'Uploaded', value: 'uploadDate' },
+        { text: 'Size', value: 'size' },
+      ],
+      uploadedDockerImages: [
+        {
+          name: 'Pi calculation',
+          uploadDate: 24,
+          size: 120,
+        },
+        {
+          name: 'Gingerbread',
+          uploadDate: 16,
+          size: 350,
+        },
+        {
+          name: 'Honeycomb',
+          uploadDate: 48,
+          size: 300,
+        },
+      ],
+    };
+  },
 };
 </script>
 
