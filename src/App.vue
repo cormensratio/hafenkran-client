25b79039e5ecc2835a4ec22dbb50549455214e60
matthiasmoke <mrauch593@googlemail.com>
2019-11-04 20:57:56 +0100

    [EPDS02-31] downgrade vuetify and install font icons

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,11 @@
 <script>
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  iconfont: 'md', // 'md' || 'mdi' || 'fa' || 'fa4'
+});
 
 export default {
   name: 'App',
