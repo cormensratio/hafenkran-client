9ef3ec651d98409067ece2174f74ad685267d9fa
Johannes Pannermayr <pannermayr@fim.uni-passau.de>
2019-11-05 10:41:48 +0100

    EPDS02-31 Added ExperimentStore to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import Vuex from 'vuex';
+import ExperimentStore from '@/store/ExperimentStore';
 import Vue from 'vue';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
+    example: ExperimentStore,
   },
 });
