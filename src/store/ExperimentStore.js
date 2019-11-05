bc7a667c4f7892342f4245f1c1802a56485fa5c9
Johannes Pannermayr <pannermayr@fim.uni-passau.de>
2019-11-05 15:24:12 +0100

    EPDS02-31 Added hardcoded JSON example to ExperimentStore for testing purposes.

--- src/store/ExperimentStore.js
+++ src/store/ExperimentStore.js
@@ -7,13 +7,15 @@ Vue.use(Vuex);
 const ExperimentStore = {
   state: {
     experiments: [{ name: 'TestDockerfile', uploadDate: '01.01.20', size: '500' }],
+    experimentsMock: [{ userId: 'c8aef4f2-92f8-47eb-bbe9-bd457f91f0e6', experimentName: 'experiment1', createdAt: '2019-11-04T12:06:16.461', size: 300 },
+      { userId: 'c8aef4f2-92f8-47eb-bbe9-bd457f91f0e6', experimentName: 'experiment2', createdAt: '2019-11-04T12:06:16.469', size: 1024 }],
   },
   getters: {
     experiments: state => state.experiments,
+    experimentsMock: state => state.experimentsMock,
   },
   mutations: {
     updateExperiments(state, experiments) {
-      debugger;
       state.experiments = experiments;
     },
   },
