00b53d712c38075ea28fc8d22d82c5f9b3a848a7
Johannes Pannermayr <pannermayr@fim.uni-passau.de>
2019-11-06 08:22:34 +0100

    EPDS02-31 Cleaned up Code

--- src/components/views/UploadedDockerfilesPage.vue
+++ src/components/views/UploadedDockerfilesPage.vue
@@ -1,8 +1,7 @@
 <template>
   <base-page>
       <template slot="body">
-        <div id="app">
-          <v-app id="inspire">
+        <div class="container">
             <v-data-table
               :headers="headers"
               :items="experimentsMock"
@@ -14,7 +13,6 @@
               <td class="text-xs-left">{{ props.item.size }}</td>
             </template>
             </v-data-table>
-          </v-app>
         </div>
       </template>
   </base-page>
@@ -31,37 +29,6 @@ export default {
   computed: {
     ...mapGetters(['experiments']),
     ...mapGetters(['experimentsMock']),
-  },
-  data() {
-    return {
-      headers: [
-        {
-          text: 'Dockerfile Name',
-          align: 'left',
-          sortable: true,
-          value: 'name',
-        },
-        { text: 'Uploaded', value: 'uploadDate', sortable: true },
-        { text: 'Size', value: 'size', sortable: true },
-      ],
-      uploadedDockerfiles: [
-        {
-          name: 'Pi calculation',
-          uploadDate: 24,
-          size: 120,
-        },
-        {
-          name: 'Gingerbread',
-          uploadDate: 16,
-          size: 350,
-        },
-        {
-          name: 'Honeycomb',
-          uploadDate: 48,
-          size: 300,
-        },
-      ],
-    };
   },
 };
 </script>
