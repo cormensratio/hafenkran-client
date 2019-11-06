b2a6ee939ecd5e65ae34ad00114022ec76b00a33
Johannes Pannermayr <pannermayr@fim.uni-passau.de>
2019-11-06 09:09:10 +0100

    EPDS02-31 Removed mocked experiments json.

--- src/store/ExperimentStore.js
+++ src/store/ExperimentStore.js
@@ -6,13 +6,10 @@ Vue.use(Vuex);
 
 const ExperimentStore = {
   state: {
-    experiments: [{ name: 'TestDockerfile', uploadDate: '01.01.20', size: '500' }],
-    experimentsMock: [{ userId: 'c8aef4f2-92f8-47eb-bbe9-bd457f91f0e6', experimentName: 'experiment1', createdAt: '2019-11-04T12:06:16.461', size: 300 },
-      { userId: 'c8aef4f2-92f8-47eb-bbe9-bd457f91f0e6', experimentName: 'experiment2', createdAt: '2019-11-04T12:06:16.469', size: 1024 }],
+    experiments: [{ experimentName: 'TestDockerfile', createdAt: '01.01.20', size: '500' }],
   },
   getters: {
     experiments: state => state.experiments,
-    experimentsMock: state => state.experimentsMock,
   },
   mutations: {
     updateExperiments(state, experiments) {
@@ -21,7 +18,7 @@ const ExperimentStore = {
   },
   actions: {
     async fetchExperiments({ commit }) {
-      const newExperiments = ApiService.doGet();
+      const newExperiments = ApiService.doGet('/experiments');
       commit('updateExperiments', newExperiments);
     },
   },
