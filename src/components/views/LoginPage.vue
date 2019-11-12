11c5ffe358187e6b260d93471c51c62b7203efc8
RGB42 <40913227+RGB@users.noreply.github.com>
2019-11-12 10:13:59 +0100

    EPDS02-96 created login template

--- src/components/views/LoginPage.vue
+++ src/components/views/LoginPage.vue
@@ -4,20 +4,20 @@
         <v-container fluid fill-height>
           <v-layout align-center justify-center>
             <v-flex xs12 sm8 md6>
-              <v-card class="elevation-24">
-                <v-toolbar color="blue">
+              <v-card class="elevation-24 shaped">
+                <v-spacer></v-spacer>
+                <v-toolbar dark color="blue">
                   <v-toolbar-title color="white">Login to Hafenkran</v-toolbar-title>
                 </v-toolbar>
                 <v-card-text>
                   <v-form ref="form" v-model="valid" lazy-validation>
-                    <v-text-field v-model="name" label="Name" prepend-icon="person" >
-                    </v-text-field>
-                    <v-text-field v-model="password" label="Password" prepend-icon="lock">
-                    </v-text-field>
+                    <v-text-field v-model="name" label="Name" prepend-icon="person" ></v-text-field>
+                    <v-text-field v-model="password"
+                                  label="Password" prepend-icon="lock"></v-text-field>
                   </v-form>
                 </v-card-text>
                 <v-card-actions justify-center>
-                  <v-btn class="btn btn-primary">Login</v-btn>
+                  <v-btn large dark color="blue">Login</v-btn>
                 </v-card-actions>
               </v-card>
             </v-flex>
