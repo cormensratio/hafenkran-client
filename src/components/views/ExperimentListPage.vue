37f5661f30beb776e430a563f02925575b621207
Johannes Pannermayr <pannermayr@fim.uni-passau.de>
2019-11-18 16:34:27 +0100

    EPDS02-93 Added input forms for Ram,Cpu and Time aswell as a Resetbutton to restore them to default.

--- src/components/views/ExperimentListPage.vue
+++ src/components/views/ExperimentListPage.vue
@@ -17,24 +17,40 @@
               </tr>
             </template>
               <template v-slot:expand="props">
-                <v-card flat>
-                  <v-card-actions>
-                    <v-layout row wrap  align-end>
-                      <v-flex xs3 offset-xs9 align-end>
-                    <b-button-group vertical>
-                    <v-btn color="blue">Reset</v-btn>
+                <v-card>
+                  <v-container fluid>
+                    <v-btn color="blue" v-on:click="resetDetails()">Reset</v-btn>
                       <v-spacer></v-spacer>
                       <v-btn color="blue">Start</v-btn>
-                    </b-button-group>
-                      </v-flex>
-                    </v-layout>
-                  </v-card-actions>
+                    <v-combobox
+                      v-model.number="selectedRam"
+                      :items="ramOptions"
+                      label="RAM"
+                      attach=""
+                      type='Number'
+                    ></v-combobox>
+                  <v-combobox
+                    v-model.number="selectedCpu"
+                    :items="cpuOptions"
+                    label="CPU Cores"
+                    attach=""
+                    type='Number'
+                  ></v-combobox>
+                    <v-combobox
+                      v-model.number="selectedTime"
+                      :items="timeOptions"
+                      label="Time in Seconds"
+                      attach=""
+                      type='Number'
+                    ></v-combobox>
+                  </v-container>
                 </v-card>
               </template>
             </v-data-table>
           </div>
           <div>
             <v-btn :href="'/newexperiment'"> Upload File </v-btn>
+            <p>{{ executionDetails }}</p>
           </div>
         </div>
       </template>
@@ -58,6 +74,14 @@ export default {
     return {
       message: '',
       expand: true,
+      selectedRam: 2,
+      ramOptions: [2, 4, 6, 8,
+      ],
+      selectedCpu: 4,
+      cpuOptions: [2, 4, 6, 8,
+      ],
+      selectedTime: 300,
+      timeOptions: [300, 360, 720, 920],
       headers: [
         {
           text: 'Dockerfile Name',
@@ -68,12 +92,37 @@ export default {
         { text: 'Uploaded', value: 'uploadDate', sortable: true },
         { text: 'Size', value: 'size', sortable: true },
       ],
+      executionDetails: [
+        {
+          experimentId: '',
+          name: '',
+          ram: 2,
+          cpu: 4,
+          time: 300,
+        },
+      ],
     };
+  },
+  watch: {
+    selectedRam() {
+      this.executionDetails[0].ram = this.selectedRam;
+    },
+    selectedCpu() {
+      this.executionDetails[0].cpu = this.selectedCpu;
+    },
+    selectedTime() {
+      this.executionDetails[0].time = this.selectedTime;
+    },
   },
   methods: {
     ...mapActions(['fetchExperiments']),
     getTimeStamp(utcTime) {
       return moment(utcTime).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    },
+    resetDetails() {
+      this.selectedRam = 2;
+      this.selectedCpu = 4;
+      this.selectedTime = 300;
     },
   },
   created() {
