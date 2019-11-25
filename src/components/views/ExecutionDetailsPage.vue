17e5cec0860d87b11126cf9c17487355a33847b0
Matthias Rauch <mrauch593@gmail.com>
2019-11-25 17:17:30 +0100

    EPDS02-123 Implemented general structure of execution details page

---
+++ src/components/views/ExecutionDetailsPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <base-page>
+    <template slot="body">
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </template>
+  </base-page>
+</template>
+
+<script>
+import { isNil } from 'lodash';
+import { mapActions } from 'vuex';
+import BasePage from '../baseComponents/BasePage';
+
+
+export default {
+  name: 'ExecutionDetailsPage',
+  components: { BasePage },
+  data() {
+    return {
+      execution: {},
+    };
+  },
+  props: {
+    executionId: String,
+  },
+  methods: {
+    ...mapActions(['getExecutionById']),
+  },
+  created() {
+    this.getExecutionById(this.executionId).then((execution) => {
+      if (!isNil(execution)) {
+        this.execution = execution;
+      }
+    });
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
