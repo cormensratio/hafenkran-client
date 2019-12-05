2d3e7effa2f02e0e51007a8bc2898c85f5931ce6
Matthias Rauch <mrauch593@gmail.com>
2019-12-05 16:39:49 +0100

    EPDS02-144 pair programming with @martin

--- src/components/views/UserRegistrationPage.vue
+++ src/components/views/UserRegistrationPage.vue
@@ -1,63 +1,60 @@
 <template>
   <base-page>
     <template slot="body">
-      <div id="app" class="custom-form">
-        <v-app>
-          <v-layout>
-            <v-flex>
-              <v-card ref="form">
-                <v-card-title>User Registration Form</v-card-title>
-                <v-card-text>
-                  <v-text-field
-                    v-model="userName"
-                    :rules="[rules.required]"
-                    :error-messages="errorMessages"
-                    label="Username"
-                    placeholder=""
-                    outline
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="userEmail"
-                    :rules="[emailRules.required, emailRules.regex]"
-                    label="Email"
-                    placeholder=""
-                    outline
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="password"
-                    :rules="[rules.required]"
-                    :type="show ? 'text' : 'password'"
-                    label="Password"
-                    placeholder=""
-                    outline
-                    hint="At least 8 characters"
-                    counter
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="confirmPassword"
-                    :rules="[rules.required, passwordConfirmationRule]"
-                    :type="show ? 'text' : 'password'"
-                    label="Confirm Password"
-                    outline
-                    hint="At least 8 characters"
-                    counter
-                    required
-                    placeholder=""
-                  ></v-text-field>
-                </v-card-text>
-                <v-divider class="mt-3"></v-divider>
-                <v-card-actions>
-                  <v-btn color="error" @click="resetForm">Clear</v-btn>
-                  <v-btn color="success" @click="submit">Submit</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-app>
-      </div>
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <v-card ref="form">
+              <v-card-title>User Registration Form</v-card-title>
+              <v-card-text>
+                <v-text-field
+                  v-model="userName"
+                  :rules="[rules.required]"
+                  :error-messages="errorMessages"
+                  label="Username"
+                  placeholder=""
+                  outline
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="userEmail"
+                  :rules="[emailRules.required, emailRules.regex]"
+                  label="Email"
+                  placeholder=""
+                  outline
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  :rules="[rules.required]"
+                  :type="show ? 'text' : 'password'"
+                  label="Password"
+                  placeholder=""
+                  outline
+                  hint="At least 8 characters"
+                  counter
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="confirmPassword"
+                  :rules="[rules.required, passwordConfirmationRule]"
+                  :type="show ? 'text' : 'password'"
+                  label="Confirm Password"
+                  outline
+                  hint="At least 8 characters"
+                  counter
+                  required
+                  placeholder=""
+                ></v-text-field>
+              </v-card-text>
+              <v-divider class="mt-3"></v-divider>
+              <v-card-actions>
+                <v-btn>Submit</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </template>
   </base-page>
 </template>
@@ -69,25 +66,25 @@ import Header from '../baseComponents/Header';
 export default {
   name: 'UserRegistrationPage',
   components: { BasePage, Header },
-  el: '#app',
-  data: () => ({
-    errorMessages: '',
-    userName: null,
-    userEmail: null,
-    password: null,
-    confirmPassword: null,
-    formHasErrors: false,
-    show: false,
-    rules: {
-      required: value => !!value || 'Required.',
-      min: v => v.length >= 8 || 'At least 8 characters',
-    },
-    emailRules: {
-      required: v => !!v || 'E-mail is required',
-      regex: v => /.+@.+/.test(v) || 'E-mail must be valid',
-    },
-  }),
-
+  data() {
+    return {
+      errorMessages: '',
+      userName: null,
+      userEmail: null,
+      password: null,
+      confirmPassword: null,
+      formHasErrors: false,
+      show: false,
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 8 || 'At least 8 characters',
+      },
+      emailRules: {
+        required: v => !!v || 'E-mail is required',
+        regex: v => /.+@.+/.test(v) || 'E-mail must be valid',
+      },
+    };
+  },
   computed: {
     form() {
       return {
@@ -109,24 +106,11 @@ export default {
   },
 
   methods: {
-    resetForm() {
-      this.errorMessages = [];
-      this.formHasErrors = false;
 
-      Object.keys(this.form).forEach((f) => {
-        this.$refs[f].reset();
-      });
-    },
   },
 };
 </script>
 
 <style>
-  .headline {
-    margin-right: auto;
-  }
-  .custom-form {
-    justify-content: center;
-    margin-left: 35%;
-  }
+
 </style>
