72abd08e487d2952d97e65b0addfac1ce0b60115
Matthias Rauch <mrauch593@gmail.com>
2019-12-05 13:21:08 +0100

    EPDS02-127 Fix unit tests

--- test/unit/specs/UploadService.spec.js
+++ test/unit/specs/UploadService.spec.js
@@ -2,13 +2,13 @@ import UploadService from '../../../src/service/UploadService';
 
 describe('UploadService', () => {
   it('should only accept files with the right file type', () => {
-    const mockFileType = 'application/x-tar';
+    const mockFileType = { type: 'application/x-tar' };
 
     expect(UploadService.checkFileType(mockFileType)).toBe(true);
   });
 
   it('should reject file names with the wrong file type', () => {
-    const mockWrongFileType = 'application/zip';
+    const mockWrongFileType = { type: 'application/zip' };
 
     expect(UploadService.checkFileType(mockWrongFileType)).toBe(false);
   });
