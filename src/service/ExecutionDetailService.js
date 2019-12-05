37321b5b3646dd3166b781291f33abed6c67b99a
Johannes Pannermayr <pannermayr@fim.uni-passau.de>
2019-12-05 14:11:24 +0100

    EPDS02-126 Added postUserIpunt method to ExectuionDetailsService to send user input via stdin.

--- src/service/ExecutionDetailService.js
+++ src/service/ExecutionDetailService.js
@@ -1,3 +1,4 @@
+import { isNil } from 'lodash';
 import { serviceUrl } from '../store/ExecutionStore';
 import ApiService from './ApiService';
 
@@ -8,5 +9,14 @@ export default class ExecutionDetailService {
       return result;
     }
     return 'No new logs';
+  }
+  static async postUserInput(userInput, executionId) {
+    let responseData;
+    if (!isNil(userInput) && !isNil(executionId)) {
+      const formData = new FormData();
+      formData.append('input', userInput);
+      responseData = await ApiService.doPost(`${serviceUrl}/executions/${executionId}/stdin`, formData);
+    }
+    return !!responseData;
   }
 }
