423aa6a28546925643faf083c2eb37ab8dfb3cda
grashuber <grashuber@fim.uni-passau.de>
2019-12-13 17:22:16 +0100

    EPDS02-159 removed file upload button from experimentlist

--- src/components/views/ExperimentListPage.vue
+++ src/components/views/ExperimentListPage.vue
@@ -3,46 +3,33 @@
     <template slot="body">
       <v-container>
         <v-layout column>
-          <v-card>
-            <v-toolbar dark color="blue">
-              <span class="title">Experiments</span>
-              <v-spacer></v-spacer>
-              <v-text-field append-icon="search"
-                            label="Search"
-                            single-line
-                            v-model="search"
-              >
-              </v-text-field>
-            </v-toolbar>
-            <v-data-table
-              :headers="headers"
-              :items="experiments"
-              :search="search"
-              class="elevation-1"
-            >
-              <template v-slot:items="props">
+          <v-flex>
+            <v-card>
+              <v-toolbar dark color="blue">
+                <span class="title">Experiments</span>
+                <v-spacer></v-spacer>
+                <v-text-field append-icon="search"
+                              label="Search"
+                              single-line
+                              v-model="search"
+                >
+                </v-text-field>
+              </v-toolbar>
+              <v-data-table
+                :headers="headers"
+                :items="experiments"
+                :search="search"
+                class="elevation-1"
+              ><template v-slot:items="props">
                 <tr @click="toggleDetails(props.item)">
                   <td class="text-xs-left">{{ props.item.name }}</td>
                   <td class="text-xs-left">{{ getTimeStamp(props.item.createdAt)}}</td>
                   <td class="text-xs-left">{{ props.item.size }} Byte</td>
                 </tr>
               </template>
-              <template v-slot:footer>
-                <v-menu
-                  v-model="menu" offset-y :close-on-content-click="false"
-                  transition="scroll-x-transition" attach="">
-                  <template v-slot:activator="{ on }">
-                    <v-btn color="blue" dark v-on="on" class="float-left mb-2">
-                      Add Experiment
-                    </v-btn>
-                  </template>
-                  <v-card>
-                    <file-upload></file-upload>
-                  </v-card>
-                </v-menu>
-              </template>
-            </v-data-table>
-          </v-card>
+              </v-data-table>
+            </v-card>
+          </v-flex>
           <v-flex v-if="showDetails">
             <div class="mt-4">
               <StartExperimentMenu @close="closeDetails"
@@ -58,17 +45,16 @@
 </template>
 
 <script>
-import { isNil } from 'lodash';
 import { mapActions, mapGetters } from 'vuex';
+import { isNil } from 'lodash';
 import BasePage from '../baseComponents/BasePage';
 import { timeStampMixin } from '../../mixins/TimeStamp';
 import StartExperimentMenu from '../baseComponents/StartExperimentMenu';
-import FileUpload from '../baseComponents/FileUpload';
 
 
 export default {
   name: 'ExperimentListPage',
-  components: { FileUpload, BasePage, StartExperimentMenu },
+  components: { BasePage, StartExperimentMenu },
   mixins: [timeStampMixin],
 
   computed: {
@@ -78,7 +64,6 @@ export default {
     return {
       search: '',
       showDetails: false,
-      menu: false,
       selectedExperiment: {},
       headers: [
         {
