8920e5a310190ce8a85b9a91e6c0860ae7ce0615
matthiasmoke <mrauch593@googlemail.com>
2019-12-15 21:24:49 +0100

    EPDS02-177 Add filters to experiment list page

---
+++ src/components/baseComponents/Filter/ExperimentFilters.vue
@@ -0,0 +1,72 @@
+<template>
+  <base-filter-component @applyFilters="applyFilters($event)"
+                         :filters="experimentFilters"
+                         @quickSearch="quickSeatch($event)"
+  >
+  </base-filter-component>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import { uniq, map } from 'lodash';
+import BaseFilterComponent from './BaseFilterComponent';
+
+export default {
+  name: 'ExperimentFilters',
+  components: { BaseFilterComponent },
+  data() {
+    return {
+      filters: {
+        name: {
+          label: 'Name',
+          value: 'name',
+          filterOptions: [],
+        },
+        size: {
+          label: 'Size',
+          value: 'size',
+          filterOptions: [],
+        },
+        owner: {
+          label: 'User',
+          value: 'owner',
+          filterOptions: [],
+        },
+      },
+    };
+  },
+  computed: {
+    ...mapGetters(['user', 'experiments', 'userList']),
+    experimentFilters() {
+      this.updateFilterOptions();
+      return this.filters;
+    },
+    nameOptions() {
+      return uniq(map(this.experiments, this.filters.name.value));
+    },
+    sizeOptions() {
+      return uniq(map(this.experiments, this.filters.size.value));
+    },
+    userOptions() {
+      return map(this.userList, user => user.name);
+    },
+  },
+  methods: {
+    updateFilterOptions() {
+      this.filters.name.filterOptions = this.nameOptions;
+      this.filters.size.filterOptions = this.sizeOptions;
+      this.filters.owner.filterOptions = this.userOptions;
+    },
+    applyFilters(appliedFilters) {
+      this.$emit('applyFilters', appliedFilters);
+    },
+    quickSearch(input) {
+      this.$emit('quickSearch', input);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/views/ExperimentListPage.vue
+++ src/components/views/ExperimentListPage.vue
@@ -5,19 +5,17 @@
         <v-layout column>
           <v-flex>
             <v-card>
-              <v-toolbar dark color="blue">
-                <span class="title">Experiments</span>
-                <v-spacer></v-spacer>
-                <v-text-field append-icon="search"
-                              label="Search"
-                              single-line
-                              v-model="search"
-                >
-                </v-text-field>
-              </v-toolbar>
+              <base-list-header title="Experiments">
+                <template slot="expansion-body">
+                  <ExperimentFilters @applyFilters="applyFilters($event)"
+                                    @quickSearch="quickSearch($event)"
+                  >
+                  </ExperimentFilters>
+                </template>
+              </base-list-header>
               <v-data-table
                 :headers="headers"
-                :items="experiments"
+                :items="filteredItems"
                 :search="search"
                 class="elevation-1"
               ><template v-slot:items="props">
@@ -50,12 +48,15 @@ import { isNil } from 'lodash';
 import BasePage from '../baseComponents/BasePage';
 import { timeStampMixin } from '../../mixins/TimeStamp';
 import StartExperimentMenu from '../baseComponents/StartExperimentMenu';
+import BaseListHeader from '../baseComponents/BaseListHeader';
+import ExperimentFilters from '../baseComponents/Filter/ExperimentFilters';
+import FilterMixin from '../../mixins/FilterMixin';
 
 
 export default {
   name: 'ExperimentListPage',
-  components: { BasePage, StartExperimentMenu },
-  mixins: [timeStampMixin],
+  components: { ExperimentFilters, BaseListHeader, BasePage, StartExperimentMenu },
+  mixins: [timeStampMixin, FilterMixin],
 
   computed: {
     ...mapGetters(['experiments']),
@@ -94,9 +95,21 @@ export default {
       this.showDetails = true;
       this.selectedExperiment = experiment;
     },
+    applyFilters(filters) {
+      if (!isNil(filters)) {
+        // Use object.assign so vue notices filters object was updated
+        this.filters = Object.assign({}, filters);
+      }
+    },
+    quickSearch(input) {
+      this.search = input;
+    },
   },
   created() {
     this.fetchExperiments();
+    this.$nextTick(() => {
+      this.items = this.experiments;
+    });
   },
 };
 </script>
