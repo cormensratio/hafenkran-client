131c42cd67d49cfb6761b92b900fbc1f011e0076
Matthias Rauch <mrauch593@gmail.com>
2019-12-18 11:58:17 +0100

    EPDS02-171 continue impl

--- src/components/baseComponents/StatisticsComponent.vue
+++ src/components/baseComponents/StatisticsComponent.vue
@@ -12,7 +12,7 @@ export default {
   name: 'StatisticsComponent',
   props: {
     type: 'csv',
-    chartData: {},
+    chartData: '',
   },
   data() {
     return {
@@ -20,22 +20,23 @@ export default {
         data: {
           name: 'TEEEST',
           format: {
-            type: '',
-            parse: '',
+            type: 'csv',
           },
-          values: [
-            { a: 'A', b: 28 }, { a: 'B', b: 55 }, { a: 'C', b: 43 },
-            { a: 'D', b: 91 }, { a: 'E', b: 81 }, { a: 'F', b: 53 },
-            { a: 'G', b: 19 }, { a: 'H', b: 87 }, { a: 'I', b: 52 },
-          ],
+          values: [],
+          // [
+            // { a: 'A', b: 28 }, { a: 'B', b: 55 }, { a: 'C', b: 43 },
+            // { a: 'D', b: 91 }, { a: 'E', b: 81 }, { a: 'F', b: 53 },
+            // { a: 'G', b: 19 }, { a: 'H', b: 87 }, { a: 'I', b: 52 },
+          // ],
         },
         mark: 'line',
-        encoding: {
-          x: { field: 'a', type: 'ordinal' },
-          y: { field: 'b', type: 'quantitative' },
-        },
       },
     };
+  },
+  watch: {
+    chartData() {
+      this.spec.data.values = this.chartData;
+    },
   },
 };
 </script>
