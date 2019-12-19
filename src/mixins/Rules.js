a7900acd58baea41b1554736f3d5d77c4a128442
Martin Loos <martinjloos@mail.de>
2019-12-19 23:59:25 +0100

    EPDS02-144 added emailRegex to Rules.js

--- src/mixins/Rules.js
+++ src/mixins/Rules.js
@@ -7,6 +7,7 @@ const RulesMixin = {
         required: value => !!value || 'Required',
         min: v => v.length >= 8 || 'Min 8 characters',
         emailMatch: () => ('The emails you entered don\'t match'),
+        emailRegex: v => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'E-mail must be valid',
       },
     };
   },
