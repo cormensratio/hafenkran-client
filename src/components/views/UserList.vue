1dd4645ba1758a5bd968047c8662f0440f1cfa4a
grashuber <grashuber@fim.uni-passau.de>
2020-01-07 15:54:38 +0100

    EPDS02-212 bug fixing

--- src/components/views/UserList.vue
+++ src/components/views/UserList.vue
@@ -1,74 +1,73 @@
 <template>
-  <div>
-    <header/>
-    <base-page>
-      <template slot="body">
-        <v-container>
-          <v-layout justify-center>
-            <div class="container">
-              <v-card max-width="800" class="mx-auto mb-5">
-                <v-toolbar dark style="background: var(--themeColor)" class="mb-4">
-                  <v-toolbar-title>
-                    Users (unaccepted)
-                  </v-toolbar-title>
-                </v-toolbar>
-                <v-list v-for="(user, index) in userList"
-                        :key="user.name" two-line>
-                  <v-list-tile class="mt-n2" v-if="user.isAccepted === ''">
-                    <v-list-tile-avatar size="50" class="mr-2">
-                      <v-avatar color="blue" size="50">
+  <base-page>
+    <template slot="body">
+      <v-container class="top">
+        <v-layout column>
+          <v-flex>
+            <v-card max-width="800" class="mx-auto mb-5">
+              <v-toolbar dark style="background: var(--themeColor)" class="mb-4">
+                <v-toolbar-title>
+                  Users (unaccepted)
+                </v-toolbar-title>
+              </v-toolbar>
+              <v-list v-for="(user, index) in userList"
+                      v-if="user.isAccepted === ''"
+                      expand
+                      :key="user.name">
+                <v-list-tile class="mt-n2">
+                  <v-list-tile-avatar size="50" class="mr-2">
+                    <v-avatar color="blue" size="50">
                         <span class="headline white--text">
                           {{ user.name.charAt(0).toUpperCase() }}
                         </span>
-                      </v-avatar>
-                    </v-list-tile-avatar>
-                    <v-list-tile-content>
-                      {{ user.name }}
-                    </v-list-tile-content>
-                    <v-list-tile-action v-if="user.isAccepted===''">
-                      <v-btn icon @click="setAccepted(index, true)">
-                        <v-icon style="color: limegreen">done</v-icon>
-                      </v-btn>
-                    </v-list-tile-action>
-                    <v-list-tile-action v-if="user.isAccepted===''">
-                      <v-btn icon @click="setAccepted(index, false)">
-                        <v-icon style="color: red">clear</v-icon>
-                      </v-btn>
-                    </v-list-tile-action>
-                  </v-list-tile>
-                  <v-divider/>
-                </v-list>
-                <v-spacer/>
-              </v-card>
-              <v-card max-width="800" class="mx-auto">
-                <v-toolbar dark style="background: var(--themeColor)" class="mb-4">
-                  <v-toolbar-title>
-                    Users
-                  </v-toolbar-title>
-                </v-toolbar>
-                <v-list v-for="(user) in userList"
-                        :key="user.name" two-line>
-                  <v-list-tile class="mt-n2" v-if="user.isAccepted">
-                    <v-list-tile-avatar size="50" class="mr-2">
-                      <v-avatar color="blue" size="50">
+                    </v-avatar>
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    {{ user.name }}
+                  </v-list-tile-content>
+                  <v-list-tile-action v-if="user.isAccepted===''">
+                    <v-btn icon @click="setAccepted(index, true)">
+                      <v-icon style="color: limegreen">done</v-icon>
+                    </v-btn>
+                  </v-list-tile-action>
+                  <v-list-tile-action v-if="user.isAccepted===''">
+                    <v-btn icon @click="setAccepted(index, false)">
+                      <v-icon style="color: red">clear</v-icon>
+                    </v-btn>
+                  </v-list-tile-action>
+                </v-list-tile>
+                <v-divider/>
+              </v-list>
+              <v-spacer/>
+            </v-card>
+            <v-card max-width="800" class="mx-auto">
+              <v-toolbar dark style="background: var(--themeColor)" class="mb-4">
+                <v-toolbar-title>
+                  Users
+                </v-toolbar-title>
+              </v-toolbar>
+              <v-list v-for="(user) in userList"
+                      v-if="user.isAccepted"
+                      :key="user.name" two-line>
+                <v-list-tile class="mt-n2">
+                  <v-list-tile-avatar size="50" class="mr-2">
+                    <v-avatar color="blue" size="50">
                         <span class="headline white--text">
                           {{ user.name.charAt(0).toUpperCase() }}
                         </span>
-                      </v-avatar>
-                    </v-list-tile-avatar>
-                    <v-list-tile-content>
-                      {{ user.name }}
-                    </v-list-tile-content>
-                  </v-list-tile>
-                </v-list>
-              </v-card>
-            </div>
-          </v-layout>
-        </v-container>
-      </template>
-    </base-page>
-    <footer/>
-  </div>
+                    </v-avatar>
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    {{ user.name }}
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </template>
+  </base-page>
 </template>
 
 <script>
@@ -81,8 +80,7 @@ export default {
   name: 'UserList',
   components: { Header, Footer, BasePage },
   data() {
-    return {
-    };
+    return {};
   },
   computed: {
     ...mapGetters(['userList']),
